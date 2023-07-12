ca320a07e1c8e7c75139586b22d675f612bfbd43
“[firstname <emmanuelajah188@gmail.com>
2023-07-12 13:47:47 +0100

    first commit

---
+++ dataTypes/dataTypes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        $speed = 3.42;
+        var_dump($speed);
+
+        $Name = "Emmanuel"; //strings 
+        $Age =  25; //number 
+        $Array = [2, 2, 3, 4, 5]; //multi-dimensional array three dimentional array
+        $boolean = true;
+        $bool = false;
+        $float = 3.55;
+        echo '<br>';
+
+
+        //object are key value pairs 
+    ?>
+</body>
+
+</html>
---
+++ variables/variable.php
@@ -0,0 +1,21 @@
+<?php
+// code runner 
+// node js
+// prettier 
+
+$Name = "Emmanuel"; //strings 
+$Age =  25; //number 
+$Array = [2, 2, 3, 4, 5]; //multi-dimensional array three dimentional array
+$boolean = true;
+$bool = false;
+
+// print($Array);
+
+echo "My name is $Name and I am  $Age years old.";
+
+var_dump($Array);
+
+
+
+
+?>
