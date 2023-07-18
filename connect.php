ca7f8971368c212a179344ae5010eeea9fbcf5b8
Styles-theandriod <59735626+Styles-Theandriod@users.noreply.github.com>
2023-07-18 14:58:51 +0100

    Add files via upload

--- connect.php
+++ connect.php
@@ -9,16 +9,40 @@ $username = '';
 $password = '';
 
 // create connection 
-$conn = new mysqli($servername, $username, $password);
+// $conn = new mysqli($servername, $username, $password);
 
 //check connection 
-if($conn->connect_error){
-    die("Connection failed: ". $conn->connect_error);
+// if($conn->connect_error){
+//     die("Connection failed: ". $conn->connect_error);
+// }else{
+//     echo "connected Successfully";
+// }
+
+
+// Or Use Tis fomart 
+
+$conn = mysqli_connect($servername, $username, $password);
+
+// check for connection 
+
+if(!$conn){
+    die("connection failed" . mysqli_connect_error());
 }else{
-    echo "connected Successfully";
+    echo "sucess";
 }
 
 
+$sql = "SELECT id, firstname, lastname FROM JOSHTBL";
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) > 0){
+    // prints each data in a row 
+    while($row = mysqli_fetch_assoc($result)){
+        echo "id: ". $row["id"]. "Name:".$row["firstname"]. "".$row["lastname"]."<br>";
+    }
+}else{
+    echo "0 results";
+}
 
 
 ?>
---
+++ statements.sql
@@ -0,0 +1,18 @@
+-- CREATE DATABASE `JoshEnterprise`;
+
+-- CREATE TABLE JOSHTBL (
+--     id INT (6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+--     firstname VARCHAR(30) NOT NULL,
+--     lastname VARCHAR(30) NOT NULL,
+--     email VARCHAR(50)
+-- );
+
+-- it limits the stored data to positive numbers and zero
+
+INSERT INTO "JOSHTBL(firstname, lastname, email) 
+Values ('Admin', 'Adamawa','Adminadmawa@gmail.com')"
+
+
+INSERT INTO JOSHTBL (firstname,lastname, email) 
+values ('Admin','namawa','Adminadmawa@gmail.com');
+
